2d64e7885fc3055aa3b95df7ba746f695164ba60
ukmrs <ukmrs@protonmail.com>
2021-03-30 18:23:16 +0200

    [trying] to implement rc4 first then proceed to rc4a

--- rothko.py
+++ rothko.py
@@ -4,15 +4,28 @@
 
 class Rothko():
     """Encryption class based on RC4A algorithm that also generates bitmaps?"""
-    statebox = list(range(256))
+    def __init__(self, key):
+        self.sb = None  # statebox
+        self._reset(key)  # looks akward but allows resetting the instance
 
     def encode(self, key: str) -> str:
         """encodes"""
         return key
 
-    def deconde(self, key: str) -> str:
+    def decode(self, key: str) -> str:
         "decodes"
         return key
 
+    def _reset(self, key):
+        """resets or initialiazes the class instance with a new key"""
+        key_len = len(key)  # len never changes
+        # accesing var is faster than calling len
+        # doesn't necesserily matter with only 256 calls but hey
+        j = 0
+        self.sb = list(range(256))
+        for i in range(256):
+            j = (j + self.sb[i] + ord(key[i % key_len])) % 256
+            self.swap(i, j)
+
     def swap(self, i, j):
-        self.statebox[i], self.statebox[j] = self.statebox[j], self.statebox[i]
+        self.sb[i], self.sb[j] = self.sb[j], self.sb[i]
