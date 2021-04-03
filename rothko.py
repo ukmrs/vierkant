a7eb3feb7abe45ace7df7e71eb79d699fef893b6
ukmrs <ukmrs@protonmail.com>
2021-04-03 22:56:57 +0200

    [cleaned] removed all ex_appendix buisness

--- rothko.py
+++ rothko.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 from rc import RC4
 import numpy as np
-import random
 from math import ceil, sqrt
 from PIL import Image
-from typing import Tuple
 from collections import deque
 from itertools import cycle
 
@@ -58,80 +56,72 @@ class Rothko():
         img.save("picture.png")
 
     def decode(self, arr):
-        ex_appendix_key = self.gen()
+        appendix_key = self.gen()
         self.arr = arr
         dim = int(self.arr.shape[0]**2)
         self.arr.resize(dim, 3)
         self.deshuffe_squares()
         mod_square = self.arr[-1].copy()
-        ex_appendix = self.decode_mod_square(mod_square, ex_appendix_key)
+        appendix = self.decode_mod_square(mod_square, appendix_key)
         self.arr.resize(dim * 3)
-        a = ex_appendix + 1
-        print(a)
-        return self.rc.decode(self.arr[:-a])
+        self.xor_gen.close()
+        return self.rc.decode(self.arr[:-appendix])
 
     def init_array(self, secret):
         encoded = np.asarray(self.rc.encode(secret), dtype=np.uint8)
         edge = calc_square_edge(len(encoded))
         appendix = edge**2 * 3 - len(encoded)
-        ex_appendix = appendix - 1
         self.arr = create_pixel_array(encoded, edge, appendix)
-        mod_square = assemble_mod_square(self.encode_mod_square(ex_appendix))
+        mod_square = assemble_mod_square(self.encode_mod_square(appendix))
         self.arr[-1] = mod_square
         print(mod_square)
         print("preshuffle\n", self.arr)
 
-    def encode_mod_square(self, ex_appendix):
-        encoded_ex_appendix = (ex_appendix ^ self.gen()) & 0xffffff
-        print(binstrip(encoded_ex_appendix))
-        return binstrip(encoded_ex_appendix)
+    def encode_mod_square(self, appendix):
+        encoded_appendix = (appendix ^ self.gen()) & 0xffffff
+        print(binstrip(encoded_appendix))
+        return binstrip(encoded_appendix)
 
     @staticmethod
-    def decode_mod_square(square, ex_appendix_key) -> int:
-        # TODO change it is so its not hacky
-        # shifts and masks for instance
-        encoded = [binstrip(byte).zfill(8) for byte in square]
-        print(encoded)
-        encoded = ''.join(encoded)
+    def decode_mod_square(square, appendix_key) -> int:
+        encoded = "".join(binstrip(byte).zfill(8) for byte in square)
         encoded = int(encoded, 2)
-
-        decoded_ex_appendix = (encoded ^ ex_appendix_key) & 0xffffff
-        return decoded_ex_appendix
+        return (encoded ^ appendix_key) & 0xffffff
 
     def calc_shuffling_amount(self, dim) -> int:
         amount = dim // 3 + 15
         return min(amount, self.max_shuffles)
 
-    def shuffle_core(self, dim, rng, shift, *args):
+    def __shuffle_core(self, dim, rng, shift, *args):
         """
         dim = amount of rows
         rng = function generating random numbers
         args = passed to range()
-        shift = in bool generator 0 to 2
+        shift = int 0 to 2
         """
         tmp = [True, True, True]
         tmp[shift] = False
-        for i, col_time in zip(range(*args), cycle(tmp)):
-            if col_time:
+        for i, row_time in zip(range(*args), cycle(tmp)):
+            if row_time:  # swaps rows
                 ix = i % dim
                 rand = rng() % dim
                 self.swap_arr_row(ix, rand)
-            else:
+            else:  # swaps columns
                 ix = i % 3
                 rand = rng() % 3
                 self.swap_arr_column(ix, rand)
 
     def shuffle_squares(self):
         dim, *_ = self.arr.shape
-        self.shuffle_core(dim, self.gen, 2,
-                          self.calc_shuffling_amount(dim) - 1, -1, -1)
+        self.__shuffle_core(dim, self.gen, 2,
+                            self.calc_shuffling_amount(dim) - 1, -1, -1)
 
     def deshuffe_squares(self):
         dim, *_ = self.arr.shape
         iterations = self.calc_shuffling_amount(dim)
         shift = iterations % 3
         swap_stack = deque(self.gen() for _ in range(iterations))
-        self.shuffle_core(dim, swap_stack.pop, shift, iterations)
+        self.__shuffle_core(dim, swap_stack.pop, shift, iterations)
 
     def swap_arr_row(self, i, j):
         self.arr[[i, j]] = self.arr[[j, i]]
@@ -164,12 +154,4 @@ class Rothko():
 
 
 if __name__ == "__main__":
-    key = 'a key of sorts'
-    appendix = 1032
-    r1, r2 = Rothko(key), Rothko(key)
-    out = assemble_mod_square(r1.encode_mod_square(appendix))
-    print(out)
-    ex_appendix_key = r2.gen()
-    a = r2.decode_mod_square(out, 0, 0, ex_appendix_key)
-    print(appendix, a)
     pass
